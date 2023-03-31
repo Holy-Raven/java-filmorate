de3c749520d4bd384b3f4961101e9beb472aa51b
Danylena <eleorex@yandex.ru>
2023-03-31 16:49:14 +0300

    2.8.5

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,9 +1,6 @@
-
 package ru.yandex.practicum.filmorate.model;
 
-
 import lombok.Data;
-
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
