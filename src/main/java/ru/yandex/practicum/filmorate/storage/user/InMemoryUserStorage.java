72e9dbd71dd65f4d8376578f320da3373265dfde
Danylena <eleorex@yandex.ru>
2023-03-31 17:06:03 +0300

    2.8.9

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.storage.user;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
-
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
