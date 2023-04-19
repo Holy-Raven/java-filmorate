13f3521371cd782b0529e900b51e08322970ae2a
Danylena <eleorex@yandex.ru>
2023-04-19 21:29:49 +0300

    4.1.0 load schema and data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,26 @@
+INSERT INTO genre (name) VALUES ('Comedy');
+
+INSERT INTO genre (name) VALUES ('Drama');
+
+INSERT INTO genre (name) VALUES ('Cartoon');
+
+INSERT INTO genre (name) VALUES ('Thriller');
+
+INSERT INTO genre (name) VALUES ('Documentary');
+
+INSERT INTO genre (name) VALUES ('Action');
+
+INSERT INTO rating (name, description)
+VALUES ('G','the film has no age restrictions');
+
+INSERT INTO rating (name, description)
+VALUES ('PG','children are recommended to watch the movie with their parents');
+
+INSERT INTO rating (name, description)
+VALUES ('PG-13','viewing is not desirable for children under 13');
+
+INSERT INTO rating (name, description)
+VALUES ('R','persons under the age of 17 can only watch the film in the presence of an adult');
+
+INSERT INTO rating (name, description)
+VALUES ('NC-17','viewing is prohibited for persons under 18 years of age');
