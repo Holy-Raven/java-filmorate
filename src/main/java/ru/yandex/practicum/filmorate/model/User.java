6646c3e9cdf55a63344b1e4434e447881f34f7f5
Danylena <eleorex@yandex.ru>
2023-05-03 20:44:26 +0300

    4.7.5 codestyle

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,11 +1,6 @@
-
 package ru.yandex.practicum.filmorate.model;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
