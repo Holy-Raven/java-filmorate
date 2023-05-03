3df7de0b832f9e46fdef2cf87f43c1d488b04f8f
Danylena <eleorex@yandex.ru>
2023-05-03 21:20:35 +0300

    4.7.9 codestyle

--- src/main/java/ru/yandex/practicum/filmorate/service/genre/GenreServiceInterface.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/genre/GenreServiceInterface.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.service.genre;
 
 import ru.yandex.practicum.filmorate.model.Genre;
 
-
 import java.util.List;
 
 public interface GenreServiceInterface {
--- src/main/java/ru/yandex/practicum/filmorate/service/mpa/MpaServiceInterface.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/mpa/MpaServiceInterface.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.service.mpa;
 
 import ru.yandex.practicum.filmorate.model.Mpa;
 import java.util.List;
+
 public interface MpaServiceInterface {
 
     List<Mpa> findAll();
